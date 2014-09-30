d9c226699a07d78ffebec17de99ebccfd3fd0216
Nihey Takizawa <nihey.takizawa@gmail.com>
2014-09-30 16:32:59 -0300

    Add flask restful dependency

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask.ext.restful import Api
 
 from sfec.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+api = Api(app, prefix='/api')
