629f07616b1a0908de1ebdb6f8434b57ef5ac7a6
Nihey Takizawa <nihey.takizawa@gmail.com>
2014-10-14 00:28:10 -0300

    Fix some PEP8 issues

--- sfec/models/category_product.py
+++ sfec/models/category_product.py
@@ -1,10 +1,11 @@
 from storm.locals import Int
 from storm.base import Storm
 
+
 class CategoryProduct(Storm):
 
-	__storm_table__ = "sfec_category_product"
-	__storm_primary__ = "category_id","product_id"
+    __storm_table__ = "sfec_category_product"
+    __storm_primary__ = "category_id", "product_id"
 
-	category_id = Int()
-	product_id = Int()
+    category_id = Int()
+    product_id = Int()
--- sfec/models/product.py
+++ sfec/models/product.py
@@ -1,19 +1,17 @@
-
 from sfec.models.base import BaseModel
 from storm.locals import Unicode, Int, Decimal, Bool, ReferenceSet
 from category_product import CategoryProduct
 
+
 class Product(BaseModel):
 
-	__storm_table__ = "sfec_product"
+    __storm_table__ = "sfec_product"
 
-	name = Unicode()
-	stock = Int(default=0)
-	description = Unicode()
-	price = Decimal()
-	is_available = Bool(default=False)
+    name = Unicode()
+    stock = Int(default=0)
+    description = Unicode()
+    price = Decimal()
+    is_available = Bool(default=False)
 
-	categories = ReferenceSet("Product.id", 
-								CategoryProduct.product_id, 
-								CategoryProduct.category_id, 
-								"Category.id")
+    categories = ReferenceSet("Product.id", CategoryProduct.product_id,
+                              CategoryProduct.category_id, "Category.id")
