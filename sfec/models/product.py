ad4a91822a99c91231faede3214a0506598117d0
Nihey Takizawa <nihey.takizawa@gmail.com>
2014-10-14 00:55:23 -0300

    Unify product and category on the same file

--- sfec/models/product.py
+++ sfec/models/product.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from storm.base import Storm
+from storm.properties import Unicode, Int, Decimal, Bool
+from storm.references import ReferenceSet
+
 from sfec.models.base import BaseModel
-from storm.locals import Unicode, Int, Decimal, Bool, ReferenceSet
-from category_product import CategoryProduct
 
 
 class Product(BaseModel):
@@ -8,10 +12,37 @@ class Product(BaseModel):
     __storm_table__ = "sfec_product"
 
     name = Unicode()
+
     stock = Int(default=0)
+
     description = Unicode()
+
     price = Decimal()
+
     is_available = Bool(default=False)
 
-    categories = ReferenceSet("Product.id", CategoryProduct.product_id,
-                              CategoryProduct.category_id, "Category.id")
+    categories = ReferenceSet('Product.id', 'CategoryProduct.product_id',
+                              'CategoryProduct.category_id', 'Category.id')
+
+
+class Category(BaseModel):
+
+    __storm_table__ = "sfec_category"
+
+    name = Unicode()
+
+    products = ReferenceSet('Category.id', 'CategoryProduct.category_id',
+                            'CategoryProduct.product_id', 'Product.id')
+
+    def __init__(self, name):
+        self.name = name
+
+
+class CategoryProduct(Storm):
+
+    __storm_table__ = "sfec_category_product"
+
+    __storm_primary__ = "category_id", "product_id"
+
+    category_id = Int()
+    product_id = Int()
