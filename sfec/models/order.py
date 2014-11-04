b6a24c398bcea5360abd10fd388c2d2550a12124
Nihey Takizawa <nihey.takizawa@gmail.com>
2014-11-03 22:05:02 -0200

    Fix some PEP8 and PyFlakes errors

--- sfec/models/order.py
+++ sfec/models/order.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
 
+from storm.references import Reference, ReferenceSet
+from storm.properties import Int, Unicode
+
+from sfec.models.user import Customer, Vendor
 from sfec.models.base import BaseModel
+
 
 class Order(BaseModel):
 
@@ -8,12 +14,13 @@ class Order(BaseModel):
     status = Unicode()
     customer_service_id = Int()
 
-    customer_service = Reference(customer_service_id, 
-                                 CustomerService.order_id)
-    products = ReferenceSet(Order.id,OrderProduct.order_id)
+    customer_service = Reference(customer_service_id,
+                                 'CustomerService.order_id')
+    products = ReferenceSet('Order.id', 'OrderProduct.order_id')
 
     def __init__(self):
         self.status = "Buying"
+
 
 class OrderProduct(BaseModel):
 
@@ -23,11 +30,12 @@ class OrderProduct(BaseModel):
     order_id = Int()
     product_id = Int()
 
-    order = Reference(order_id, Order.id)
-    product = Reference(product_id, Product.id)
+    order = Reference(order_id, 'Order.id')
+    product = Reference(product_id, 'Product.id')
 
-    def __init__(self,product):
+    def __init__(self, product):
         self.product = product
+
 
 class CustomerService(BaseModel):
 
@@ -45,4 +53,3 @@ class CustomerService(BaseModel):
         self.order_id = order.id
         self.customer_id = customer.id
         self.vendor_id = vendor.id
-
