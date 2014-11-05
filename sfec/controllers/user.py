2f800ce014e4abf76330b92d84258f70c788519a
BrOrlandi <brorlandi@gmail.com>
2014-11-05 16:59:05 -0200

    WIP commit

--- sfec/controllers/user.py
+++ sfec/controllers/user.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import re
 from flask import Blueprint, g, session, request
 
 from sfec.models.user import *
@@ -7,11 +8,42 @@ from sfec.database.runtime import get_default_store
 
 user_api = Blueprint('user_api', __name__)
 
+def is_email_address_valid(email):
+    """Validate the email address using a regex."""
+    if not re.match("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", email):
+        return False
+    return True
+
+@user_api.route("/register", methods=['POST'])
+def register():
+	"""Register a new user."""
+	if not is_email_address_valid(request.form['email']):
+		return "E-mail not valid!"
+
+	store = get_default_store()
+	# Default user is customer, an admin must change the user to staff member (admin or vendor)
+	found = store.find(User, User.email == request.form['email']).one()
+	if found is not None:
+		return "E-mail already taken!"
+	new_customer = Customer()
+	user = User()
+	user.name = request.form['name']
+	user.email = request.form['email']
+	user.set_password(request.form['password'])
+	user.birth_date = datetime.strptime(request.form['birth_date'],"%m-%d-%Y")
+	store = get_default_store()
+	new_customer.user = user
+	store.add(new_customer)
+	store.commit()
+	return "Success!"
+
+
+
 @user_api.route('/login', methods=['POST'])
 def login():
 	"""Log the user in."""
 	store = get_default_store()
-	user = User.authenticate(store, request.form['usermail'],request.form['password'])
+	user = User.authenticate(store, request.form['email'],request.form['password'])
 	if user:
 		session['user'] = user.id
 		return "True"
@@ -21,3 +53,8 @@ def login():
 def logout():
 	session.pop('user',None)
 	return "True"
+
+@user_api.route('/users/<int:user_id>/set_vendor')
+def set_vendor(user_id):
+	store = get_default_store()
+	
